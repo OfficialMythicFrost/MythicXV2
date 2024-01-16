5cf0e83d0212aec9a422a7c6e5ff71bc185a7697
Mythic Frost <77963571+Mythic-Frost@users.noreply.github.com>
2024-01-16 20:27:10 +0530

    Update askgpt.js

--- src/commands/utility/askgpt.js
+++ src/commands/utility/askgpt.js
@@ -1,7 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js');
 const puppeteer = require('puppeteer'); //RUN: npm install puppeteer in terminal
+
 module.exports = {
-data: new SlashCommandBuilder()
-.setName('askgpt')
-.setDescription('Ask ChatGpt!')
-.addStringOption(option => option.setName('query').setDescription('The query for the AI!').setRequired(true)),
+    data: new SlashCommandBuilder()
+   .setName('askgpt')
+   .setDescription('Ask ChatGpt!')
+   .addStringOption(option => option.setName('query').setDescription('The query 
+    for the AI!').setRequired(true)),
+  
+   async execute (interaction) {
+      await interaction.reply({ content: 'typing...', ephemeral: true })
+   }
+}
