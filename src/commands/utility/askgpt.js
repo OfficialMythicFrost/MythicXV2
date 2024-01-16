f81ffc6eb8a269e1e151e4de5815780b0f904ff5
Mythic Frost <77963571+Mythic-Frost@users.noreply.github.com>
2024-01-16 20:50:31 +0530

    Update askgpt.js

--- src/commands/utility/askgpt.js
+++ src/commands/utility/askgpt.js
@@ -10,5 +10,28 @@ module.exports = {
   
    async execute (interaction) {
       await interaction.reply({ content: 'typing...', ephemeral: true })
-   }
+
+      const { option } = interaction;
+      const query = options.getString('query');
+      const browser = await puppeteer.launch({ headless: true });
+      const page await browser.newPage();
+      await page.goto('https://chat-app-f2d296.zapier.app/');
+      const textBoxSelector = 'textarea[aria-label="chatbot-user-prompt")';
+      await page.waitForSelector(textBoxSelector);
+      await page.type(textBoxSelector, query);
+      await page.keyboard.press('Enter');
+      await page.waitForSelector("[data-testid="final-bot-response") p');
+      let value = await page. Seval("[data-testid="final-bot-response"1", async (elements) => {
+          return elements.map((element) => element.textContent);
+      });
+      setTimeout(async () => {
+          if (value.length == 0) return await interaction.editReply({content: 'Error!'));
+      }, 30000)
+       
+      await browser.close();
+      value.shift();
+      await interaction.editReply({ content: $(value.join("\n\n\n\n')}})
+
+                                                          
+    }
 }
